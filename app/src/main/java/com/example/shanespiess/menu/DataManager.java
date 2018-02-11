a10dc58eb0e38007d5cc622e52a52e019ac90545
Shane Spiess <spiess3@yahoo.com>
2018-02-11 17:56:18 -0500

    some minor refactory of datamanager

--- app/src/main/java/com/example/shanespiess/menu/DataManager.java
+++ app/src/main/java/com/example/shanespiess/menu/DataManager.java
@@ -10,9 +10,9 @@ import java.util.List;
 
 public class DataManager {
     private static DataManager ourInstance = null;
-    private List<MenuCategory> mCategories = new ArrayList<>();
-    private List<MenuItem> mMenuItems = new ArrayList<>();
-    private HashMap<Integer,List<MenuItem>> mCategoryMenuItems = new HashMap<>();
+    private HashMap<Integer,MenuCategory> mMenuCategoryMap = new HashMap<>();
+    private HashMap<Integer,List<MenuItem>> mCategoryMenuItemsMap = new HashMap<>();
+    private HashMap<Integer,MenuItem> mMenuItemsMap = new HashMap<>();
 
     public static DataManager getInstance(){
         if (ourInstance == null) {
@@ -23,65 +23,69 @@ public class DataManager {
     }
 
     public List<MenuCategory> getMenuCategories() {
-        return mCategories;
+        return new ArrayList<>(mMenuCategoryMap.values());
     }
 
     public List<MenuItem> getMenuItems(Integer categoryId) {
-        return mCategoryMenuItems.get(categoryId);
+        if (!mCategoryMenuItemsMap.containsKey(categoryId))
+            return null;
+
+        return mCategoryMenuItemsMap.get(categoryId);
     }
 
     public MenuItem getMenuItem(int itemId) {
-        MenuItem menuItem = null;
+        if (!mMenuItemsMap.containsKey(itemId))
+            return null;
 
-        for (MenuItem item : mMenuItems) {
-            if (item.getItemId() == itemId) {
-                menuItem = item;
-                break;
-            }
-        }
-
-        return menuItem;
+        return mMenuItemsMap.get(itemId);
     }
 
     public MenuCategory getMenuCategory(int categoryId) {
-        MenuCategory menuCategory = null;
-
-        for (MenuCategory category : mCategories) {
-            if (category.getMenuCategoryId() == categoryId) {
-                menuCategory = category;
-                break;
-            }
+        if (!mMenuCategoryMap.containsKey(categoryId)) {
+            return null;
         }
-
-        return menuCategory;
+        return mMenuCategoryMap.get(categoryId);
     }
 
     private void initializeMenuData() {
         MenuCategory sandwiches = new MenuCategory(1, "Sandwiches");
-        mCategories.add(sandwiches);
-        List<MenuItem> sandwichItems = new ArrayList<>();
-        sandwichItems.add(new MenuItem(1,R.drawable.grilledcheese,"Grilled Cheese", sandwiches, "The best grilled cheese ever."));
-        sandwichItems.add(new MenuItem(2,R.drawable.hotdog,"Hot Dog", sandwiches, "All beef Hot Dog."));
-        sandwichItems.add(new MenuItem(3,R.drawable.hamburger,"Hamburger", sandwiches, "Healthier than it looks."));
-        sandwichItems.add(new MenuItem(4,R.drawable.chicken,"Chicken", sandwiches, "A great chicken sandwich."));
-        mCategoryMenuItems.put(sandwiches.getMenuCategoryId(), sandwichItems);
-        mMenuItems.addAll(sandwichItems);
+        mMenuCategoryMap.put(1, sandwiches);
+        ArrayList<MenuItem> sandwichItems = new ArrayList<>();
+        MenuItem grilledCheese = new MenuItem(1,R.drawable.grilledcheese,"Grilled Cheese", sandwiches, "The best grilled cheese ever.");
+        sandwichItems.add(grilledCheese);
+        mMenuItemsMap.put(1, grilledCheese);
+        MenuItem hotDog = new MenuItem(2,R.drawable.hotdog,"Hot Dog", sandwiches, "All beef Hot Dog.");
+        mMenuItemsMap.put(2, hotDog);
+        sandwichItems.add(hotDog);
+        MenuItem hamburger = new MenuItem(3,R.drawable.hamburger,"Hamburger", sandwiches, "Healthier than it looks.");
+        mMenuItemsMap.put(3,hamburger);
+        sandwichItems.add(hamburger);
+        MenuItem chicken = new MenuItem(4,R.drawable.chicken,"Chicken", sandwiches, "A great chicken sandwich.");
+        mMenuItemsMap.put(4, chicken);
+        sandwichItems.add(chicken);
+        mCategoryMenuItemsMap.put(1, sandwichItems);
 
         MenuCategory salads = new MenuCategory(2, "Salads");
-        mCategories.add(salads);
-        List<MenuItem> saladItems = new ArrayList<>();
-        saladItems.add(new MenuItem(5,R.drawable.caesar,"Caesar Salad", salads, "Just a regular Ceasar Salad."));
-        saladItems.add(new MenuItem(6,R.drawable.sidesalad,"Side Salad", salads, "Not so bad."));
-        mCategoryMenuItems.put(salads.getMenuCategoryId(), saladItems);
-        mMenuItems.addAll(saladItems);
+        mMenuCategoryMap.put(2, salads);
+        ArrayList<MenuItem> saladItems = new ArrayList<>();
+        MenuItem caesar = new MenuItem(5,R.drawable.caesar,"Caesar Salad", salads, "Just a regular Ceasar Salad.");
+        saladItems.add(caesar);
+        mMenuItemsMap.put(5, caesar);
+        MenuItem side = new MenuItem(6,R.drawable.sidesalad,"Side Salad", salads, "Not so bad.");
+        saladItems.add(side);
+        mMenuItemsMap.put(6, side);
+        mCategoryMenuItemsMap.put(2, saladItems);
 
         MenuCategory sides = new MenuCategory(3, "Sides");
-        mCategories.add(sides);
-        List<MenuItem> sideItems = new ArrayList<>();
-        sideItems.add(new MenuItem(7,R.drawable.frenchfries,"French Fries", sides, "Everyone loves French Fries."));
-        sideItems.add(new MenuItem(8,R.drawable.bakedpotato,"Baked Potato", sides, "Good old baked potato."));
-        mCategoryMenuItems.put(sides.getMenuCategoryId(), sideItems);
-        mMenuItems.addAll(sideItems);
+        mMenuCategoryMap.put(3, sides);
+        ArrayList<MenuItem> sideItems = new ArrayList<>();
+        MenuItem frenchFries = new MenuItem(7,R.drawable.frenchfries,"French Fries", sides, "Everyone loves French Fries.");
+        sideItems.add(frenchFries);
+        mMenuItemsMap.put(7, frenchFries);
+        MenuItem bakedPotato = new MenuItem(8,R.drawable.bakedpotato,"Baked Potato", sides, "Good old baked potato.");
+        sideItems.add(bakedPotato);
+        mMenuItemsMap.put(8, bakedPotato);
+        mCategoryMenuItemsMap.put(3, sideItems);
     }
 
     private DataManager() {}
